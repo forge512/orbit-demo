70f7466b92f7320396c5d8796456ec2ab1b2c2ca
Chris Bonser <bonser.chris@gmail.com>
2022-06-20 15:05:45 -0500

    Increase complexity via forks to try recreating removed from cache errors

--- app/components/planet-list.js
+++ app/components/planet-list.js
@@ -2,26 +2,26 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { dropTask } from 'ember-concurrency';
+import { action } from '@ember/object';
 
 export default class PlanetListComponent extends Component {
   @service store;
   @tracked staticPlanets = [];
+  @tracked displayMoons = false;
 
   get planets() {
     return this.store.cache.liveQuery((q) => q.findRecords('planet'));
   }
 
+  @action
+  toggleMoons() {
+    this.displayMoons = !this.displayMoons;
+  }
+
   /* eslint require-yield: off */
   @dropTask
   *refresh() {
-    this.staticPlanets = this.store.cache.findRecords('planet');
-  }
-
-  @dropTask
-  *remove(planet) {
     let fork = this.store.fork();
-    yield fork.removeRecord(planet);
-    yield this.store.merge(fork);
-    fork.destroy();
+    this.staticPlanets = fork.cache.findRecords('planet');
   }
 }
